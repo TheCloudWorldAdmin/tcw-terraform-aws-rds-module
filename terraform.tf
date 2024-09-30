e5af7231556b4bde0f637a60441379f9c071a4a2
TheCloudWorldAdmin <kwilshan@gmail.com>
2024-09-30 21:22:45 +0530

    msg

--- terraform.tf
+++ terraform.tf
@@ -1,1 +1,12 @@
-hello world
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tcw-tf-12345-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
