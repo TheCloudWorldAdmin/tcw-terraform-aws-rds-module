827b9a773fb8b5f522d42c5213fccaf8d1c26547
TheCloudWorldAdmin <kwilshan@gmail.com>
2024-09-30 22:07:23 +0530

    s3

--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-tcw-tf-12345-test-bucket"
+  bucket = "my-tcw-tf-00000-test-bucket"
 
   tags = {
     Name        = "My bucket"
