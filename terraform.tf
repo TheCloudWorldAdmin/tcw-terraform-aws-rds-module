5a6d72fccd43ec07dfde46c20576667ccaaa248b
TheCloudWorldAdmin <kwilshan@gmail.com>
2024-10-01 19:25:21 +0530

    added remote backend

--- terraform.tf
+++ terraform.tf
@@ -2,13 +2,35 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket   = "tcw-terrafomr-state-lock"
+    key      = "${var.env}/s3-bucket/terraform.tfstate"
+    region   = "us-east-1"
+  }
+}
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name = "terraform-state-lock-dynamo"
+  hash_key = "LockID"
+  read_capacity = 20
+  write_capacity = 20
+ 
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 resource "aws_s3_bucket" "example" {
   bucket = "my-tcw-tf-00000-test-bucket"
 
   tags = {
     Name        = "My bucket"
-    Environment = "Dev"
+    Environment = var.env
   }
 }
+
+variable "env" {}
 
 #testing
